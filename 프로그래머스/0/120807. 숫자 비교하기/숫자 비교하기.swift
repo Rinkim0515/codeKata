aff0c1355e9fc0bdb5e33778990bb041ff8b7c1e
bunabloom <168620795+bunabloom@users.noreply.github.com>
2024-05-28 10:37:58 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    var result = 0
+    if num1 == num2 {
+        result = 1
+    } 
+    else  {
+
+    result = -1
+    } 
+    
+    return result
+}
