26e79e524c8a3007423e3eb56e8b34c595198906
Bloom <168620795+bunabloom@users.noreply.github.com>
2024-06-10 09:40:10 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
@@ -1,11 +1,3 @@
 func solution(_ s:String) -> String {
-    var temp : [String] = s.map{String($0)}
-    var result : String = ""
-    
-    temp.sort(by: >)
-    
-    result = temp.joined(separator: "")
-    
-    return result
+    return ''
 }
-
