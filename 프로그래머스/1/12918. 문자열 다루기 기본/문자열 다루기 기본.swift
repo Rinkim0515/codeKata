d7f79144c9c5db52ce1b1acb71aedfa84b6b81bf
Bloom <168620795+bunabloom@users.noreply.github.com>
2024-06-11 21:18:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -0,0 +1,12 @@
+func solution(_ s:String) -> Bool {
+    
+    if (s.count == 4 || s.count == 6) && Int(s) != nil {
+        return true
+        
+    } else {
+        return false
+    }
+    
+    
+   
+}
