c1d4762c921f60c53ffd55344e5f59ce902a2bd1
Bloom <168620795+bunabloom@users.noreply.github.com>
2024-06-18 09:53:42 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -0,0 +1,12 @@
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 { return a}
+    else { return gcd(b, a % b)}
+    
+}
+func lcm(_ a: Int, _ b: Int) -> Int {
+    a * b / (gcd(a, b))
+}
+
+func solution(_ n:Int, _ m:Int) -> [Int] {
+     return [gcd(n, m), lcm(n, m)]
+}
