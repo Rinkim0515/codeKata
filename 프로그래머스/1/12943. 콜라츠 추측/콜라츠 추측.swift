4395cf9d688226d4b7da6923f613d9420ea0c29d
Bloom <168620795+bunabloom@users.noreply.github.com>
2024-06-28 13:00:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -1,29 +1,12 @@
 func solution(_ num:Int) -> Int {
+    guard num != 1 else{ return 0 }
     var count = 0
     var temp = num
-    while temp != 1  {
-        
-        if temp % 2 == 0{
-            temp = temp / 2
-            if count < 500 {
-                count += 1
-            } else {
-                count = -1
-                break
-            }
-            
-        }else if temp % 2 != 0 {
-            temp = (temp * 3) + 1
-             if count < 500 {
-                count += 1
-            } else {
-                 count = -1
-                break
-            }
-        }
-        
-        
-    }
     
-    return count
+    while temp != 1  {
+        temp = temp % 2 == 0 ? temp/2 : (temp * 3) + 1
+        count += 1
+        if count > 500 { break }
+    }
+    return count > 500 ? -1 : count
 }
