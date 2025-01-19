c68b174d02d0dc4b35694d9c7f3627b97b339a5c
RinKim <kimrindev@gmail.com>
2025-01-19 21:02:31 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,12 @@
+def solution(myString):
+    answer = ''
+    for i in myString.lower():
+        if i == 'a':
+            answer += 'A'
+            continue
+        
+        answer += i
+        
+        
+        
+    return answer
