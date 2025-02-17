de0ebef0ad9aec6b37ea0e710fd6b47cbe4341b8
RinKim <kimrindev@gmail.com>
2025-02-17 14:27:59 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = -1
+    for (index,number) in enumerate(num_list):
+        if number < 0:
+            answer = index
+            break
+         
+        
+        
+        
+    return answer
