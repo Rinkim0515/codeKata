5287e941fa96766587fa779bf02402de0a208fea
RinKim <kimrindev@gmail.com>
2025-02-20 11:56:04 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 19.99 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -0,0 +1,12 @@
+def solution(arr, queries):
+    answer = []
+    for (s, e, k) in queries:
+        min_value =  float('inf')
+        for i in range(s, e + 1):
+            if arr[i] > k:
+                min_value = min(min_value, arr[i])
+        answer.append(min_value if min_value != float('inf') else -1 )
+            
+        
+        
+    return answer
