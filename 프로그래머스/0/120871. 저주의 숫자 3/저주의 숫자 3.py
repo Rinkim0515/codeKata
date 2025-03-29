433390a324fe16d65e4dfeb6701e42d18b8013e7
RinKim <kimrindev@gmail.com>
2025-03-29 09:17:54 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.04 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    count = 0
+    while count < n:
+        answer += 1
+        if answer % 3 != 0 and '3' not in str(answer):
+            count += 1
+        
+
+    return answer
