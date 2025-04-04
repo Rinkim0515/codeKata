f77eb7f1f5262f369c9d3415a694a4e7085e9c6f
RinKim <kimrindev@gmail.com>
2025-04-04 19:13:56 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    d.sort()
+    answer = 0
+    for i in d:
+        if budget - i >= 0:
+            budget -= i
+            answer += 1
+        else:
+            break
+    return answer
